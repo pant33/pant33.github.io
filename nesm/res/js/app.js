7463eb0918323d103d8b356ab838e83b384c3ae7
pant33 <546605883@qq.com>
2020-06-18 14:03:29 +0800

    t1

--- nesm/res/js/app.js
+++ nesm/res/js/app.js
@@ -124,6 +124,26 @@ window.global_vue = new Vue({
     data() {
         return {
             tp: 'prod',
+            t1: '<van-swipe-cell>\
+            <template #left>\
+                <van-button square type="primary" text="选择" />\
+            </template>\
+            <van-cell :border="false" title="单元格" value="内容" />\
+            <template #right>\
+                <van-button square type="danger" text="删除" />\
+                <van-button square type="primary" text="收藏" />\
+            </template>\
+        </van-swipe-cell>',
+            t2: '<van-swipe-cell>\
+            <template #left>\
+                <van-button square type="primary" text="选择" />\
+            </template>\
+            <template #right>\
+                <van-button square type="danger" text="删除"></van-button>\
+                <van-button square type="primary" text="收藏"></van-button>\
+            </template>\
+            <van-cell :border="false" title="单元格" value="内容" />\
+        </van-swipe-cell>',
         }
     },
     mounted() {
